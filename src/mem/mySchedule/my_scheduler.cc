9a8172a1669fa8144b55fed7443091114b70c701
Tanawin <umd-dev01-connect@outlook.com>
2022-06-13 09:25:56 +0700

    [mem] update rr

--- src/mem/mySchedule/my_scheduler.cc
+++ src/mem/mySchedule/my_scheduler.cc
@@ -30,12 +30,21 @@ namespace mySchedule
         
         gem5::memory::MemPacketQueue::iterator minRR_iter  = queue.end();
         gem5::memory::MemPacketQueue::iterator minALL_iter = queue.end();
+
+        ContexParam                            readyNumNext= INT_MAX;
+        ContexParam                            readyNumTurn= INT_MAX;
         
         for (auto memIter = queue.begin(); memIter != queue.end(); memIter++ ){
             // convert to memory packet
             gem5::memory::MemPacket* memPkt = *memIter;
             //context  id 
             ContexParam cpr = memPkt->pkt->schedulerCID;
+
+            readyNumTurn = std::min(readyNumTurn, cpr);
+            if( (cpr > lastRR) && (cpr < readyNumNext) ){
+                readyNumNext = cpr;
+            }
+            ///////////////////// this session is for packet ready
             // for larger context ID that have lowest context id
             if (owner->packetReady(memPkt)){
                 if ((cpr > lastRR) && (cpr < minRR)){
@@ -47,22 +56,33 @@ namespace mySchedule
                     minALL     = cpr;
                 }
             }
+            //////////////////////////////////////////////////////////
         }
         
-        if (minRR != INT_MAX){
+        if ( readyNumNext != INT_MAX ){
             // do next context first
-            DPRINTF(scheduler,"now we select next cid %d\n", minRR);
-            lastRR = minRR;
-            return minRR_iter;
-        }else if (minALL != INT_MAX){
+            if (minRR == readyNumNext){
+                DPRINTF(scheduler,"now we select next cid %d\n", minRR);
+                lastRR = minRR;
+                return minRR_iter;
+            }else{
+                DPRINTF(scheduler,"there are next thread that want to schedule but it consequent thread can't issue request\n");
+                return queue.end();
+            }
+        }else if ( (readyNumTurn <= lastRR ) && (readyNumTurn != INT_MAX) ){
             // if not switch back to first lowest context number
-            DPRINTF(scheduler,"now we have loop to start cid %d\n", minALL);
-            lastRR = minALL;
-            assert(minALL_iter != queue.end());
-            return minALL_iter;
+            if (minALL == readyNumTurn){
+                DPRINTF(scheduler,"now we have loop to start cid %d\n", minALL);
+                lastRR = minALL;
+                //assert(minALL_iter != queue.end());
+                return minALL_iter;
+            }else{
+                DPRINTF(scheduler,"there are loop back thread that want to schedule but it can't issue request\n");
+                return queue.end();
+            }
         }else{
             // In case there are no mem packets to schedule
-            DPRINTF(scheduler,"there are no things to select ");
+            DPRINTF(scheduler,"there are no things to select \n");
             return queue.end();
         }
     }
